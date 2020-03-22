10e0511f72301d4cb46104e08051907ea6f172d3
Didier <didorg@gmail.com>
2020-03-22 16:32:21 -0400

    Using application.yml
    spring boot kafka multiple consumer

--- src/main/java/com/dido/kafkaconsumer/listener/KafkaConsumer.java
+++ src/main/java/com/dido/kafkaconsumer/listener/KafkaConsumer.java
@@ -1,20 +1,29 @@
 package com.dido.kafkaconsumer.listener;
 
+import com.dido.kafkaconsumer.model.Transfer;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "kafka-example", groupId = "group_id")
+    private final Gson gson;
+
+    @Autowired // This annotation is NOT necessary, but it is always convenient
+    public KafkaConsumer(Gson gson) {
+        this.gson = gson;
+    }
+
+     @KafkaListener(topics = "kafka-example", groupId = "group_id")
     public void consumeMessage(String message){
         System.out.println("Consumed message: " + message);
     }
 
-    //todo: add @KafkaListener for Transfer object
-   /* @KafkaListener(topics = "kafka-transfer", groupId = "group_json")
-    public void consumeJson(Transfer transfer){
-        System.out.println("Consumed transfer: " + transfer);
-    }*/
+    @KafkaListener(topics = "kafka-transfer")
+    public void consumeJson(String transfer) {
+        Transfer model = gson.fromJson(String.valueOf(transfer), Transfer.class);
+        System.out.println("Model converted value: " + model);
+    }
 }
